12ed55cecb1e1ad30ae36f14e49260a11ed7c077
Your Name <you@example.com>
2026-04-16 16:47:35 +0530

    cors error fix

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 if __name__ == "__main__":
     port = int(os.getenv("FLASK_PORT", 3006))
     debug = os.getenv("FLASK_ENV", "production").lower() == "development"
